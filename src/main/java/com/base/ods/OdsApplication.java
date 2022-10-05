5a893158f4ccc19fd53417d5fe39af8aa5cef390
Oguzhan Ulusoy <oguzhan.ulusoy@orioninc.com>
2022-10-05 11:32:58 +0300

    initial commit

---
+++ src/main/java/com/base/ods/OdsApplication.java
@@ -0,0 +1,13 @@
+package com.base.ods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdsApplication.class, args);
+	}
+
+}
